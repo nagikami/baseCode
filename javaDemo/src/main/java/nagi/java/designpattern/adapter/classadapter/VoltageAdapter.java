9bac86c531ff13013a033dccfd7d1d902af857e2
nagikami <jiuhaokami@gmail.com>
2022-05-27 14:38:54 +0800

    add correct word

--- javaDemo/src/main/java/nagi/java/designpattern/adapter/classadapter/VoltageAdapter.java
+++ javaDemo/src/main/java/nagi/java/designpattern/adapter/classadapter/VoltageAdapter.java
@@ -3,7 +3,7 @@ package nagi.java.designpattern.adapter.classadapter;
 /**
  * 类适配器模式
  * 通过继承src类，实现dst接口完成src->dst的转换
- * 缺点 需要继承src dst必须是接口 src方法都会暴露在adaper
+ * 缺点 需要继承src dst必须是接口 src方法都会暴露在adapter
  * 优点 可以重写src方法，增加灵活性
  */
 public class VoltageAdapter extends Voltage220 implements IVoltage5 {
